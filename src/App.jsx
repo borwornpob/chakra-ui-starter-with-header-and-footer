5a1b521ca2eb4530a2a63a80d89590a025f002a8
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2023-10-24 08:49:56 +0700

    update footer to be at the bottom

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Flex direction="column" minH="100vh">
+            <Box flex="1">
+                <Router>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Router>
+            </Box>
+            <Box>
+                <Footer />
+            </Box>
+        </Flex>
+    );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,67 +1,69 @@
 import {
-  ButtonGroup,
-  Container,
-  IconButton,
-  Stack,
-  Text,
-  Image,
+    ButtonGroup,
+    Container,
+    IconButton,
+    Stack,
+    Text,
+    Image,
 } from "@chakra-ui/react";
 import * as React from "react";
 import {
-  FaGithub,
-  FaLinkedin,
-  FaTwitter,
-  FaInstagram,
-  FaLine,
+    FaGithub,
+    FaLinkedin,
+    FaTwitter,
+    FaInstagram,
+    FaLine,
 } from "react-icons/fa";
+import useWindowDimensions from "../helper/dimensions";
 
 export default function Footer() {
-  return (
-    <Container
-      as="footer"
-      position="relative"
-      bottom="0"
-      role="contentinfo"
-      py={{
-        base: "2",
-        md: "2",
-      }}
-    >
-      <Stack>
-        <Stack justify="space-between" direction="row" align="center">
-          <Text>Borwornpob.</Text>
-          <ButtonGroup variant="ghost">
-            <IconButton
-              as="a"
-              onClick={() => {
-                location.href = "https://instagram.com/first_bfr";
-              }}
-              aria-label="Instagram"
-              icon={<FaInstagram fontSize="1.25rem" />}
-            />
-            <IconButton
-              as="a"
-              onClick={() => {
-                location.href = "https://line.me/R/ti/p/%40cud_sapa";
-              }}
-              aria-label="Line"
-              icon={<FaLine fontSize="1.25rem" />}
-            />
-            <IconButton
-              as="a"
-              onClick={() => {
-                location.href = "https://github.com/borwornpob";
-              }}
-              aria-label="GitHub"
-              icon={<FaGithub fontSize="1.25rem" />}
-            />
-          </ButtonGroup>
-        </Stack>
-        <Text fontSize="sm" color="subtle">
-          &copy; {new Date().getFullYear()} Borwornpob Thumrongchotikhun. All
-          rights reserved.
-        </Text>
-      </Stack>
-    </Container>
-  );
+    const { width, height } = useWindowDimensions();
+    return (
+        <Container
+            as="footer"
+            position="relative"
+            bottom="0"
+            role="contentinfo"
+            maxW="100%"
+            py="1rem"
+        >
+            <Stack>
+                <Stack justify="space-between" direction="row" align="center">
+                    <Text>Borwornpob.</Text>
+                    <ButtonGroup variant="ghost">
+                        <IconButton
+                            as="a"
+                            onClick={() => {
+                                location.href =
+                                    "https://instagram.com/first_bfr";
+                            }}
+                            aria-label="Instagram"
+                            icon={<FaInstagram fontSize="1.25rem" />}
+                        />
+                        <IconButton
+                            as="a"
+                            onClick={() => {
+                                location.href =
+                                    "https://line.me/R/ti/p/0982893299";
+                            }}
+                            aria-label="Line"
+                            icon={<FaLine fontSize="1.25rem" />}
+                        />
+                        <IconButton
+                            as="a"
+                            onClick={() => {
+                                location.href = "https://github.com/borwornpob";
+                            }}
+                            aria-label="GitHub"
+                            icon={<FaGithub fontSize="1.25rem" />}
+                        />
+                    </ButtonGroup>
+                </Stack>
+                <Text fontSize="sm" color="subtle">
+                    &copy; {new Date().getFullYear()} Borwornpob
+                    Thumrongchotikhun. All rights reserved.
+                </Text>
+            </Stack>
+        </Container>
+    );
 }
